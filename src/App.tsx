17f37199ea02b1489393dd3558c8779dedd00a90
its-dk <itsdkdev@outlook.com>
2024-11-15 12:07:34 +0530

    shadcn added

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import './App.css';
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+
+//data will be the string we send from our server
+const apiCall = () => {
+  axios.get('http://localhost:8080').then((data) => {
+    //this console.log will be in our frontend console
+    console.log(data)
+  })
+}
+
+function App() {
+  return (
+    <div className="App">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+
+      <header className="App-header">
+
+        <button className="text-3xl font-bold underline" onClick={apiCall}>Make API Call</button>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
